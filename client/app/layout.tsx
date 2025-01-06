d23e41030d9febcddd849a3b5715a22a75fb1250
Abdulwahab <wahab210899@gmail.com>
2025-01-06 16:25:40 +0300

    Modifications (alot)

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,10 +1,20 @@
+import '@/app/ui/global.css';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${poppins.variable}`}>
       <body>{children}</body>
     </html>
   );
