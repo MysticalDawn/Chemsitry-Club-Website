ed69caaa8dc3453a0ecd756658488e01dbcc67c8
Mazen01 <115363747+Mazen01@users.noreply.github.com>
2025-03-21 20:38:24 +0300

    about 0.1

--- client/app/(nav-others)/about_us/fourth-part/org-chart.tsx
+++ client/app/(nav-others)/about_us/fourth-part/org-chart.tsx
@@ -4,11 +4,13 @@ import Image from "next/image";
 export default function OrgChart() {
   return (
     <section className="py-16 bg-gray-100 ">
-      <h5 className="text-1xl font-bold text-center text-amber-600">-Members</h5>
+      <h5 className="text-1xl font-bold text-center text-amber-600">
+        -Members
+      </h5>
       <h2 className="text-3xl font-bold text-center mb-8">Club 2023/2024</h2>
       <div className="flex justify-center mb-8">
         <Image
-          src="/home/MemChart.png"
+          src="/home/MemChart1.jpeg"
           alt="KFUPM Chemical Engineering Club Organizational Chart"
           width={1000}
           height={600}
