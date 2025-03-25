4e65354a622245ae6ff3f3799257802f48e2ef90
Omar Alfawaz <Omar12345Alfawaz@gmail.com>
2025-03-26 02:06:01 +0300

    jm

--- client/app/(nav-others)/calendar/page.tsx
+++ client/app/(nav-others)/calendar/page.tsx
@@ -1,6 +1,6 @@
 export default function Calender() {
   return (
-    <div>
+    <div style={{ height: '100vh' , width: '100vh', display:'flex' , alignContent:'center', justifyContent: 'center'}}>
       <h2>Calendar Page</h2>
       {/* Add your calendar content here */}
     </div>
