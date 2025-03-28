c16d7f0cee4c0e1535136e459d0dfb754de112b5
Abdulwahab <wahab210899@gmail.com>
2025-03-28 05:52:19 +0300

    Change to file strecture

--- client/app/(nav-others)/calendar/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Calender() {
-  return (
-    <div style={{ height: '100vh' , width: '100vh', display:'flex' , alignContent:'center', justifyContent: 'center'}}>
-      <h2>Calendar Page</h2>
-      {/* Add your calendar content here */}
-    </div>
-  );
-}
