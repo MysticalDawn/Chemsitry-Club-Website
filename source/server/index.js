c5139a3c876edd920016ac47438a15370fe50f79
Abdulwahab <wahab210899@gmail.com>
2025-03-28 06:03:48 +0300

    Some changes for deployment

--- source/server/index.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js"; // Correct import for Supabase
-import dotenv from "dotenv";
-dotenv.config();
-console.log(process.env.SUPABASE_KEY)
-
-const supabaseUrl = "https://vpcnzxqkoahqotklvctk.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
