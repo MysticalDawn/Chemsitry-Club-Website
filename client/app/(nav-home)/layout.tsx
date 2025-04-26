08420b53a19cf8b81e297b6b3e33814006502d3b
Abdulwahab <wahab210899@gmail.com>
2025-04-27 02:44:31 +0300

    minor fix for the footer

--- client/app/(nav-home)/layout.tsx
+++ client/app/(nav-home)/layout.tsx
@@ -1,12 +1,7 @@
-'use client';
 import "@/app/ui/global.css";
 import { Poppins } from "next/font/google";
-import NavbarCustom from "../components/desktop/navbar-custom";
-import NavbarCustomMobile from "../components/mobile/navbar-mobile";
 import "bootstrap/dist/css/bootstrap.min.css";
 import kfupm from "../../public/home/kfupm.jpeg";
-import { useEffect, useState } from "react";
-
 const poppins = Poppins({
   subsets: ["latin"],
   display: "swap",
@@ -19,39 +14,22 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 765);
-    };
-
-    // Initial check
-    handleResize();
-
-    // Add event listener
-    window.addEventListener("resize", handleResize);
-
-    // Cleanup event listener
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  const containerStyle = {
-    height: "100vh",
-    width: "100vw",
-    overflow: "hidden",
-    backgroundImage: `url(${kfupm.src})`,
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-  };
-
   return (
     <html lang="en" className={`${poppins.variable}`}>
       <body>
-        <div style={containerStyle}>
-          {isMobile ? <NavbarCustomMobile /> : <NavbarCustom />}
-          {children}
+        <div
+          className="main-nav po"
+          style={{
+            backgroundImage: `url(${kfupm.src})`,
+            height: "100vh",
+            width: "100vw",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            marginRight: "1rem",
+          }}
+        >
+          {children}  
         </div>
       </body>
     </html>
--- client/app/(nav-home)/page.tsx
+++ client/app/(nav-home)/page.tsx
@@ -1,11 +1,32 @@
+'use client';
 import Home from "./first-part/main-nav";
 import OfferSection from "./second-part/offer-section";
 import { Container } from "react-bootstrap";
 import Footer from "../components/desktop/footer";
 import Events from "./third-part/events";
+import { useEffect, useState } from "react";
+import NavbarCustomMobile from "../components/mobile/navbar-mobile";
+import NavbarCustom from "../components/desktop/navbar-custom";
 export default function Page() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 765);
+    };
+
+    // Initial check
+    handleResize();
+
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup event listener
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
     <Container className="m-0 p-0 w-100 h-100 min-vw-100">
+      {isMobile ? <NavbarCustomMobile /> : <NavbarCustom />}
       <Home />
       <OfferSection />
       <Events />
